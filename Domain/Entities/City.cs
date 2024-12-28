7c890dd525add8f1c4c8e3416fa3b4b7b7506611
a7iiid <naz131681@gmail.com>
2024-12-28 23:38:34 +0200

    start with main entity

---
+++ Domain/Entities/City.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Domain.Entities
+{
+    public class City
+    {
+        public Guid CityID { get; set; }
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public int PostOfficeCode { get; set; }
+        public IList<Hotel> Hotels { get; set; }
+
+    }
+}
