a0d0904863f3745bfb2d376aec718adf44383f52
a7iiid <naz131681@gmail.com>
2024-12-29 18:47:16 +0200

    add Review enteti

---
+++ Domain/Entities/Review.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace Domain.Entities
+{
+    public class Review
+    {
+        public Guid Id { get; set; }
+        public Guid BookingId { get; set; }
+        public string Comment { get; set; }
+        public DateTime ReviewDate { get; set; }
+        public float Rating { get; set; }
+    }
+}
