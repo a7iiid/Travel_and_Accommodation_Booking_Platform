271a566073a0d54da64e7cc3b7c731075ed309d9
a7iiid <naz131681@gmail.com>
2024-12-30 18:07:36 +0200

    CRUD Reposetory

---
+++ Domain/Interfaces/CRUDReposetory.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace Domain.Interfaces
+{
+    internal interface CRUDReposetory<T>
+    {
+        Task Add(T  entity);
+        Task<T> GetById(Guid id);
+        Task<IEnumerable<T>> GetAll();
+        Task<bool>Delete(Guid id);
+        Task<bool>Exists(Guid id);
+        Task<T>Update(T entity);
+       
+    }
+}
