e27b54f31f2061ee239b9a8d7939b25cb49f4f52
a7iiid <naz131681@gmail.com>
2024-12-31 16:58:13 +0200

    DesignTimeDbContextFactory to fixe migration error "Unable to create a 'DbContext' of type 'RuntimeType'"

---
+++ Infrastructure/DB/DesignTimeDbContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Infrastructure.DB
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            optionsBuilder
+                .UseSqlServer("Data Source=NAZZAL;Initial Catalog=RestaurantReservationCore;User ID=ahmad;Password=12345678;Connect Timeout=30;Encrypt=True;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False ");
+
+            return new ApplicationDbContext(optionsBuilder.Options);
+        }
+    }
+}
