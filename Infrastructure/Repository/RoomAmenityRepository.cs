90ded7924ab78d56f7a0b4e292e14b9bb9add89e
a7iiid <naz131681@gmail.com>
2025-01-08 21:21:58 +0200

    room amainty repository

---
+++ Infrastructure/Repository/RoomAmenityRepository.cs
@@ -0,0 +1,57 @@
+﻿
+
+using Domain.Entities;
+using Domain.Model;
+using Infrastructure.DB;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace Infrastructure.Repository
+{
+    public class RoomAmenityRepository:Repository<RoomAmenity>
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly ILogger<RoomAmenityRepository> _logger;
+
+        public RoomAmenityRepository(ApplicationDbContext context, ILogger<RoomAmenityRepository> logger)
+            :base(context,logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        public async Task<PaginatedList<RoomAmenity>> GetAllAsync(string? searchQuery, int pageNumber, int pageSize)
+        {
+            try
+            {
+                var query = _context.RoomAmenities.AsQueryable();
+                var totalItemCount = await query.CountAsync();
+                var pageData = new PageData(totalItemCount, pageSize, pageNumber);
+
+                if (!string.IsNullOrWhiteSpace(searchQuery))
+                {
+                    searchQuery = searchQuery.Trim();
+                    query = query.Where
+                    (city => city.Name.Contains(searchQuery) ||
+                    city.Description.Contains(searchQuery));
+                }
+
+                var result = query
+                    .Skip(pageSize * (pageNumber - 1))
+                    .Take(pageSize)
+                    .AsNoTracking()
+                    .ToList();
+
+                return new PaginatedList<RoomAmenity>(result, pageData);
+            }
+            catch (Exception)
+            {
+                _logger.LogWarning("Data Access Exception");
+                return new PaginatedList<RoomAmenity>(new List<RoomAmenity>(), new PageData(0, 0, 0));
+            }
+        }
+
+        
+    }
+
+}
