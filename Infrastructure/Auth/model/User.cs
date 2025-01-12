38762903353d921f9fe0f75f41a049f62bb15da0
a7iiid <naz131681@gmail.com>
2025-01-12 21:02:08 +0200

    add auth user and user model

---
+++ Infrastructure/Auth/model/User.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace Infrastructure.Auth.model
+{
+    public record User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool IsAdmin { get; set; }
+        public string Salt { get; set; }
+    }
+}
---
+++ Infrastructure/Auth/user auth/AuthUser.cs
@@ -0,0 +1,38 @@
+﻿using Infrastructure.Auth.model;
+using Infrastructure.DB;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Infrastructure.Auth
+{
+    public class AuthUser : IAuthUser
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AuthUser(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<User?> GetUserAsync(string email)
+        {
+            var user = await _context
+                .Users
+                .SingleOrDefaultAsync(u => u.Email.Equals(email));
+
+            if (user is null)
+            {
+                return null;
+            }
+            return new User
+            {
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Email = user.Email,
+                Password = user.PasswordHash,
+                IsAdmin = user.isAdmin,
+                Salt = user.Salt
+            };
+        }
+    }
+}
