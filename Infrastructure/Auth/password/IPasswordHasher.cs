dd1572ff15f783f12e55dbc84201198a40fc515c
a7iiid <naz131681@gmail.com>
2025-01-13 20:42:41 +0200

    password hashing

---
+++ Infrastructure/Auth/password/IPasswordHasher.cs
@@ -0,0 +1,9 @@
+﻿namespace Infrastructure.Auth.password
+{
+    public interface IPasswordHasher
+    {
+        string GenerateSalt();
+        string HashPassword(string password, string salt);
+        bool VerifyPassword(string password, string salt, string storedHash);
+    }
+}
---
+++ Infrastructure/Auth/password/PasswordHasher.cs
@@ -0,0 +1,37 @@
+﻿
+using System.Security.Cryptography;
+using System.Text;
+
+
+namespace Infrastructure.Auth.password
+{
+    public class PasswordHasher : IPasswordHasher
+    {
+        public string GenerateSalt()
+        {
+            byte[] saltBytes = new byte[16];
+            using (var rng = new RNGCryptoServiceProvider())
+            {
+                rng.GetBytes(saltBytes);
+            }
+            return Convert.ToBase64String(saltBytes);
+        }
+
+        public string HashPassword(string password, string salt)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                var saltedPassword = password + salt;
+                var saltedPasswordBytes = Encoding.UTF8.GetBytes(saltedPassword);
+                var hashBytes = sha256.ComputeHash(saltedPasswordBytes);
+                return Convert.ToBase64String(hashBytes);
+            }
+        }
+
+        public bool VerifyPassword(string password, string salt, string storedHash)
+        {
+            var hash = HashPassword(password, salt);
+            return hash == storedHash;
+        }
+    }
+}
