5fb1eec8ba62c5e37ca984e1ab2df557b149f148
a7iiid <naz131681@gmail.com>
2025-01-19 16:47:02 +0200

    end cityServices

--- Application/Services/CityServices.cs
+++ Application/Services/CityServices.cs
@@ -31,5 +31,32 @@ namespace Application.Services
                 pageNumber,
                 pageSize);
         }
+        public async Task<City> GetCityByIdAsync(Guid cityId)
+        {
+            var city = await _cityRepository.GetByIdAsync(cityId);
+            if (city == null)
+                throw new KeyNotFoundException($"City with ID {cityId} not found.");
+
+            return city;
+        }
+        public async Task AddCityAsync(City city)
+        {
+            if (city == null)
+                throw new ArgumentNullException(nameof(city));
+
+            await _cityRepository.AddUserAsync(city);
+        }
+        public async Task UpdateCityAsync(City city)
+        {
+            if (city == null)
+                throw new ArgumentNullException(nameof(city));
+
+            await _cityRepository.UpdateAsync(city);
+        }
+
+        public async Task<bool> DeleteCityAsync(Guid cityId)
+        {
+            return await _cityRepository.DeleteAsync(cityId);
+        }
     }
 }
