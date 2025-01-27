0e9a35705a83b4d7068c0b3ca6d12945457747f9
a7iiid <naz131681@gmail.com>
2025-01-27 17:38:55 +0200

    create BookingProfile

---
+++ Application/profile/BookingProfile.cs
@@ -0,0 +1,18 @@
+﻿
+
+using Application.DTOs.BookingDTOs;
+using AutoMapper;
+using Domain.Entities;
+
+namespace Application.profile
+{
+    public class BookingProfile:Profile
+    {
+        public BookingProfile()
+        {
+            CreateMap<Booking, BookingDTO>();
+
+            CreateMap<BookingDTO, Booking>();
+        }
+    }
+}
