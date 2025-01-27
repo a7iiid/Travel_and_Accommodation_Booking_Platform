565ac9022d5126e264298a02d9a14df2dbd4f57a
a7iiid <naz131681@gmail.com>
2025-01-27 17:18:17 +0200

    add BookingDTO

---
+++ Application/DTOs/BookingDTOs/BookingDTO.cs
@@ -0,0 +1,18 @@
+﻿
+
+using Domain.Entities;
+
+namespace Application.DTOs.BookingDTOs
+{
+    public record BookingDTO
+    {
+        public Guid UserId { get; set; }
+        public DateTime CheckInDate { get; set; }
+        public DateTime CheckOutDate { get; set; }
+        public DateTime BookingDate { get; set; }
+        public double Price { get; set; }
+        public Review? Review { get; set; }
+        public Payment? Payment { get; set; }
+        public User User { get; set; }
+    }
+}
