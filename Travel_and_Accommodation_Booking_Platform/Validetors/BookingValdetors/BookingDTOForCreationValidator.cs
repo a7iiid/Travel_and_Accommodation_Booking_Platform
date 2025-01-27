582f1750fd1ec8bb42e686c518cb9a0e0f73912a
a7iiid <naz131681@gmail.com>
2025-01-27 20:37:51 +0200

    add BookingDTOForCreationValidator

---
+++ Travel_and_Accommodation_Booking_Platform/Validetors/BookingValdetors/BookingDTOForCreationValidator.cs
@@ -0,0 +1,41 @@
+﻿using FluentValidation;
+using Application.DTOs.BookingDTOs;
+
+namespace Application.Validators
+{
+    public class BookingDTOForCreationValidator : AbstractValidator<BookingDTOForCreation>
+    {
+        public BookingDTOForCreationValidator()
+        {
+            // Rule for RoomId
+            RuleFor(booking => booking.RoomId)
+                .NotEmpty().WithMessage("RoomId is required.")
+                .NotEqual(Guid.Empty).WithMessage("RoomId must be a valid GUID.");
+
+            // Rule for UserId
+            RuleFor(booking => booking.UserId)
+                .NotEmpty().WithMessage("UserId is required.")
+                .NotEqual(Guid.Empty).WithMessage("UserId must be a valid GUID.");
+
+            // Rule for CheckInDate
+            RuleFor(booking => booking.CheckInDate)
+                .NotEmpty().WithMessage("CheckInDate is required.")
+                .GreaterThanOrEqualTo(DateTime.Today).WithMessage("CheckInDate must be today or in the future.");
+
+            // Rule for CheckOutDate
+            RuleFor(booking => booking.CheckOutDate)
+                .NotEmpty().WithMessage("CheckOutDate is required.")
+                .GreaterThan(booking => booking.CheckInDate).WithMessage("CheckOutDate must be after CheckInDate.");
+
+            // Rule for BookingDate
+            RuleFor(booking => booking.BookingDate)
+                .NotEmpty().WithMessage("BookingDate is required.")
+                .LessThanOrEqualTo(DateTime.Today).WithMessage("BookingDate cannot be in the future.");
+
+            // Rule for Price
+            RuleFor(booking => booking.Price)
+                .NotEmpty().WithMessage("Price is required.")
+                .GreaterThan(0).WithMessage("Price must be greater than 0.");
+        }
+    }
+}
