85f6c3b5a64b17ca6a389f8e4bac2add8195552e
a7iiid <naz131681@gmail.com>
2025-02-01 14:22:33 +0200

    create PaymentProfile

---
+++ Application/profile/PaymentProfile.cs
@@ -0,0 +1,17 @@
+﻿using Application.DTOs.PaymentDTOs;
+using AutoMapper;
+using Domain.Entities;
+
+
+namespace Application.profile
+{
+    public class PaymentProfile:Profile
+    {
+        public PaymentProfile()
+        {
+            CreateMap<PaymentDTO,Payment >()
+                .ForMember(dest => dest.Id, opt => opt.Ignore()).ReverseMap();
+            
+        }
+    }
+}
