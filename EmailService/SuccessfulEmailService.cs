e9d7e0091648ff5795f4e96d1569417bc8719371
a7iiid <naz131681@gmail.com>
2025-02-19 18:51:32 +0200

    start with SuccessfulEmailService

---
+++ EmailService/SuccessfulEmailService.cs
@@ -0,0 +1,21 @@
+﻿using System.Net.Mail;
+using System.Net;
+using Microsoft.Extensions.Configuration;
+
+namespace EmailService
+{
+    public class SuccessfulEmailService
+    {
+        private readonly IConfiguration _configuration;
+
+        public SuccessfulEmailService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            
+        }
+
+
+
+
+    }
+}
