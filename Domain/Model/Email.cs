f6d76dcf62633c3aa736a642ce0b1f23280ba2fc
a7iiid <naz131681@gmail.com>
2025-02-19 18:53:10 +0200

    add email model

---
+++ Domain/Model/Email.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace Domain.Model
+{
+    public class Email
+    {
+        public string FirstName { get; set; }
+        public Guid BookingId { get; set; }
+        public decimal Amount { get; set; }
+        public string ToEmail { get; set; }
+    }
+}
