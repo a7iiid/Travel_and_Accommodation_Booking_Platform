23b534e03808d530fb37174cd55a9e4bae4bff90
a7iiid <naz131681@gmail.com>
2025-02-19 16:55:52 +0200

    start with UserServiceTests

---
+++ TABPTesting/UserServiceTests.cs
@@ -0,0 +1,23 @@
+﻿
+
+using AutoMapper;
+using Domain.Interfaces;
+using Moq;
+
+namespace TABPTesting
+{
+    public class UserServiceTests
+    {
+        private readonly Mock<IUserRepository> _mockUserRepository;
+        private readonly Mock<IPasswordHasher> _mockPasswordHasher;
+        private readonly Mock<IMapper> _mockMapper;
+
+        public UserServiceTests()
+        {
+            _mockUserRepository = new Mock<IUserRepository>();
+            _mockPasswordHasher = new Mock<IPasswordHasher>();
+            _mockMapper = new Mock<IMapper>();
+        }
+
+    }
+}
